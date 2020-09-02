d55f84bc37f0ac615e1d74f786baa28b94f4de70
Naeun <knae11@naver.com>
2020-09-02 11:40:39 +0900

    version1

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sayHi = (name, age, gender) => {
+    return `hello ${name}, you are age of ${age}, you are ${gender}.`;
+};
+console.log(sayHi("nauni", 5, "f"));
+//# sourceMappingURL=index.js.map
