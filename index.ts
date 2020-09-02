d0d59ab45d3508f090854905e303f47e0a0818a8
Naeun <knae11@naver.com>
2020-09-02 14:24:35 +0900

    complete

--- index.ts
+++
@@ -1,6 +0,0 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `hello ${name}, you are age of ${age}, you are ${gender}.`;
-};
-
-console.log(sayHi("nauni", 5, "f"));
-export {};
